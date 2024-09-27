5a0319ab78f69ce2555a1f2194f86aadb28a6aa1
Dian Gao <diangao@umich.edu>
2024-09-27 15:33:12 -0400

    Finish Part1

---
+++ __jac_gen__/server.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from jaclang.plugin.feature import JacFeature as _Jac
+from jaclang.plugin.builtin import *
+from dataclasses import dataclass as __jac_dataclass__
+from jaclang import jac_import as __jac_import__
+import typing as _jac_typ
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact(_Jac.Walker):
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, world!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact_with_body(_Jac.Walker):
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class greet_user(_Jac.Walker):
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, ' + self.name + '! How can I assist you today?'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class get_time(_Jac.Walker):
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        if _jac_typ.TYPE_CHECKING:
+            import time
+        else:
+            time, = __jac_import__(target='time', base_path=__file__, lng='py', absorb=False, mdl_alias=None, items={})
+        current_time = time.strftime('%H:%M:%S')
+        _Jac.report({'response': 'The current time is ' + current_time})
